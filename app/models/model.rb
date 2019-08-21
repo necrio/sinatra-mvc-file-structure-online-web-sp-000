7d11b89330d9f7d593d922917a2a102f7fa3f4c6
Keith Isham <kisham8899@gmail.com>
2019-08-21 23:55:48 +0000

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,23 @@
-class Model
-
-end
+  class Dog
+    
+    attr_reader :breed 
+    attr_accessor :name, :age 
+    
+    @@dogs = [] 
+    
+    def initialize(name, breed, age)
+      @name = name
+      @breed = breed 
+      @age = age 
+      @@dogs << self 
+    end 
+    
+    def self.all 
+      @@dogs 
+    end 
+      
+    
+    
+    
+    
+  end
